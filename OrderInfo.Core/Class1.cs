fbf1c9cdeaeffd87434f0e64a4415bc4b9d7a05e
https://github.com/pltosman <polatosmann@gmail.com>
2021-07-22 16:15:55 +0100

    gitignore file added

---
+++ OrderInfo.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OrderInfo.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ OrderInfo.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OrderInfo.DataAccess
+{
+    public class Class1
+    {
+    }
+}
